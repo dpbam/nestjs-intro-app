5be1348fd51691b2a703593ddb78830eceedf6fc
dpbam <derekphtpt@gmail.com>
2022-04-06 17:14:55 -0600

    trying to fix quotes on save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProductsModule } from "./products/products.module";
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      "mongodb+srv://thederekphelps:f1ddl35t1ck5!@cluster0.kbze5.mongodb.net/nestjs-demo?retryWrites=true&w=majority"
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
